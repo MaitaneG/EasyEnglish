d69d8dbd21b823eebbd37f8d150f514892237d68
MaitaneG <Maitane>
2022-12-26 21:56:50 +0100

    Login, register, games

--- EasyEnglish/src/app/app-routing.module.ts
+++ EasyEnglish/src/app/app-routing.module.ts
@@ -19,6 +19,10 @@ const routes: Routes = [
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
   },
+  {
+    path: 'games',
+    loadChildren: () => import('./games/games.module').then( m => m.GamesPageModule)
+  },
 ];
 
 @NgModule({
