a7c99126bf122066865db0365ecc1a878bc294f4
MaitaneG <Maitane>
2023-01-04 20:42:40 +0100

    Start with the games

--- EasyEnglish/game/urls.py
+++ EasyEnglish/game/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     path('game/',views.game,name='game'),
+    path('game/hangman/',views.hangman,name='hangman'),
     path('logout/',views.logout,name='logout'),
 ]
--- EasyEnglish/game/views.py
+++ EasyEnglish/game/views.py
@@ -5,6 +5,9 @@ from django.contrib.auth import logout as auth_logout
 def game(request):
     return render(request, 'game.html')
 
+def hangman(request):
+    return render(request, 'hangman.html')
+
 def logout(request):
     auth_logout(request)
     return render(request, 'index.html')
