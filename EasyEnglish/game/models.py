580c19d6073853de19770f23fc81ee592da1449f
MaitaneG <Maitane>
2023-01-07 17:06:30 +0100

    Start entering information

--- EasyEnglish/game/models.py
+++ EasyEnglish/game/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Hangman(models.Model):
+    TYPES=[('Animal', 'Animal'),('Food','Food'), ('Furniture', 'Furniture'), ('School material', 'School material')]
+
+    id = models.AutoField(primary_key=True)
+    word = models.CharField(max_length=50)
+    type = models.CharField(max_length=50, choices=TYPES)
+    spanish = models.CharField(max_length=100)
+
+    def __str__(self):
+        return '%s' % self.word
+
