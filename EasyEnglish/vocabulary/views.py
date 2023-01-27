611a517087b05a09f6ee5c7afb4b01f08f94504f
MaitaneG <Maitane>
2023-01-27 20:28:24 +0100

    Order the data displayed

--- EasyEnglish/vocabulary/views.py
+++ EasyEnglish/vocabulary/views.py
@@ -5,11 +5,13 @@ from django.http import JsonResponse
 
 # Create your views here.
 def vocabulary(request):
-    hangman=Hangman.objects.all();
+    hangman=Hangman.objects.all()
+    hangman=hangman.order_by("word")
     return render(request, 'vocabulary.html', {'vocabulary':hangman})
 
 def clasifiedVocabulary(request):
     types=request.POST.get("types")
     print(types)
-    hangman=list(Hangman.objects.filter(types=types).values());
+    hangman=Hangman.objects.filter(types=types);
+    hangman=list(hangman.order_by("word").values())
     return JsonResponse(hangman,safe=False)
