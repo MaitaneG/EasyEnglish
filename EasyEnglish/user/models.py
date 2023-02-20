690a79ba1a74d149e06eaf24061c9743e3268c3e
MaitaneG <Maitane>
2023-02-20 13:22:50 +0100

    Start saving users learnt words

--- EasyEnglish/user/models.py
+++ EasyEnglish/user/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from vocabulary.models import Term
+
 # Create your models here.
 class Ranking(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     points=models.IntegerField()
+    learnt=models.ManyToManyField(Term)
 
     def __str__(self):
         return '%s' % self.user
