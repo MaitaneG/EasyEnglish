59680b222b289f7dc5badda2b8e416c7ad0517b7
MaitaneG <Maitane>
2023-04-16 13:51:20 +0200

    Mysql erabili

--- EasyEnglish/EasyEnglish/settings.py
+++ EasyEnglish/EasyEnglish/settings.py
@@ -79,9 +79,16 @@ WSGI_APPLICATION = 'EasyEnglish.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    ###'default': {
+    ###    'ENGINE': 'django.db.backends.sqlite3',
+    ###    'NAME': BASE_DIR / 'db.sqlite3',
+    ##},
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'easyenglish',
+        'USER': 'root',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
